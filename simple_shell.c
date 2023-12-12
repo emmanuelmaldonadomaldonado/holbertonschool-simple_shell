37c511e7acf3aaa0fe254a3aab035b52d7528f8a
Louis Toro <louistororosario@gmail.com>
2023-12-12 15:51:32 -0400

    we wrote our getenv() function, now we have the full size of path, and isatty function added

--- simple_shell.c
+++ simple_shell.c
@@ -6,34 +6,69 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
-#define my_args 100
+extern char **environ;
+
+#define my_args 100 /*Define the maximum number of arguments*/
+
+char *_getenv(const char *name)
+{
+	char *result = NULL;
+	char *current_var;
+	char **env;
+
+	for (env = environ; *env != NULL; env++)
+	{
+		current_var = *env;
+		if (strncmp(current_var, name, strlen(name)) == 0 && current_var[strlen(name)] == '=')
+			result = malloc(strlen(current_var) - strlen(name));
+		if (result != NULL)
+		{
+			strcpy(result, current_var + strlen(name) + 1);
+			break;
+		}
+	}
+	return (result);
+}
+
+
+void imprimir_prompt()
+{
+	if (isatty(fileno(stdin)))
+	{
+		printf("$ ");
+		fflush(stdout);
+	}
+}
 
 int main(void)
 {
 	int num_token, k;
-	char *argv[my_args];
-	char *command = NULL;
+	size_t i;
+	char *argv[my_args]; /*Array to store command arguments*/
+	char *command = NULL; /*Input command string*/
 	char *token;
 	size_t buffer = 0;
 	size_t length;
-	char *path = "/usr/bin/";
-	char my_path[my_args];
+	char *path[] = {"/usr/local/bin/", "/usr/bin/", "/bin/", "/usr/local/games/", "/usr/games/"}; /*Default path for command execution*/
+	char my_path[my_args]; /*Store the constructed path for command execution*/
 	int status;
 	pid_t pid;
+	char **env = environ; /*Copy of the environment variables*/
+	size_t path_size = sizeof(path) / sizeof(path[0]);
 
 	while (1)
 	{
-		printf("$ ");
-		if (getline(&command, &buffer, stdin) != -1)
+		imprimir_prompt();
+		if (getline(&command, &buffer, stdin) != -1) /*Read command from user input*/
 		{
 			length = strlen(command);
 			if (command[length - 1] == '\n')
 				command[length - 1] = '\0';
 
-			if (strcmp(command, "") == 0)
+			if (strcmp(command, "") == 0) /*Check for empty command*/
 				continue;
 
-			if (strcmp(command, "exit") == 0)
+			if (strcmp(command, "exit") == 0) /*Check for "exit" command to terminate the shell*/
 			{
 				free(command);
 				return 0;
@@ -41,42 +76,45 @@ int main(void)
 
 			num_token = 0;
 			token = strtok(command, " ");
-			while (token != NULL && num_token < my_args - 1)
+			while (token != NULL && num_token < my_args - 1) /*Tokenize the command into arguments*/
 			{
 				argv[num_token++] = token;
 				token = strtok(NULL, " ");
 			}
 			argv[num_token] = NULL;
 
-			if (num_token == 0)
+			if (num_token == 0) /*Check for no arguments provided*/
 				continue;
 
 			for (k = 0; k < num_token; k++)
-				/*{
-				  printf("%s ", argv[k]);
-				  }
-				  printf("\n");*/
+				;
 
-				strcpy(my_path, path);
-			strcat(my_path, argv[0]);
+			for (i = 0; i < path_size; i++)
+			{
+				strcpy(my_path, path[i]); /*Construct the full path for the command*/
+				strcat(my_path, argv[0]);
+
+				if (access(my_path, X_OK) == 0) /*Check if the command is executable*/
+				{
+					break;
+				}
+			}
 
 			if (access(my_path, X_OK) == -1)
 			{
-				/*perror("access");*/
-				printf("Command not found or lack of execute permissions\n");
+				printf("Command not found1\n");
 				continue;
 			}
-
-			pid = fork();
+			pid = fork(); /*Fork a new process for command execution*/
 			if (pid == -1)
 			{
 				perror("fork failed");
 				exit(EXIT_FAILURE);
 			}
 
-			if (pid == 0)
+			if (pid == 0) /*Child process executes the command*/
 			{
-				if (execve(my_path, argv, NULL) == -1)
+				if (execve(my_path, argv, env) == -1)
 				{
 					printf("command not found\n");
 					exit(EXIT_FAILURE);
@@ -84,10 +122,10 @@ int main(void)
 			}
 			else
 			{
-				waitpid(pid, &status, 0);
+				waitpid(pid, &status, 0); /* Parent process waits for child process to complete */
 			}
 		}
-		else
+		else /* If input is not read properly, break the loop */
 		{
 			printf("\n");
 			break;
