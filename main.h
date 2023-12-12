0836e94da7a579ba037fe9d58b178e57e73c9c98
Louis Toro <louistororosario@gmail.com>
2023-12-12 18:11:27 -0400

    prototype for isatty removed

--- main.h
+++ main.h
@@ -1,11 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
 extern char **environ;
 
 #define my_args 100
 
 char *_getenv(const char *name);
-void imprimir_prompt(void);
 
 #endif /* MAIN_H */
