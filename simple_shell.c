214fdac6027beedaa934c6035f884f8d5a519bf9
Louis Toro <louistororosario@gmail.com>
2023-12-12 18:12:17 -0400

    testing intranet

--- simple_shell.c
+++ simple_shell.c
@@ -1,34 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
+#include "main.h"
 
 extern char **environ;
 
 #define my_args 100 /*Define the maximum number of arguments*/
-
-char *_getenv(const char *name)
-{
-	char *result = NULL;
-	char *current_var;
-	char **env;
-
-	for (env = environ; *env != NULL; env++)
-	{
-		current_var = *env;
-		if (strncmp(current_var, name, strlen(name)) == 0 && current_var[strlen(name)] == '=')
-			result = malloc(strlen(current_var) - strlen(name));
-		if (result != NULL)
-		{
-			strcpy(result, current_var + strlen(name) + 1);
-			break;
-		}
-	}
-	return (result);
-}
 
 
 void imprimir_prompt(int interactive)
