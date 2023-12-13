3652844a19f52d55247ef82d8407fa37b93d960a
Louis Toro <louistororosario@gmail.com>
2023-12-13 09:23:39 -0400

    test for intranet

--- get_env.c
+++ get_env.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-char *_getenv(const char *name)
+char *_getenv(const char *PATH)
 {
 	char *result = NULL;
 	char *current_var;
@@ -10,11 +10,16 @@ char *_getenv(const char *name)
 	for (env = environ; *env != NULL; env++)
 	{
 		current_var = *env;
-		if (strncmp(current_var, name, strlen(name)) == 0 && current_var[strlen(name)] == '=')
-			result = malloc(strlen(current_var) - strlen(name));
+		if (strncmp(current_var, PATH, strlen(PATH)) == 0 && current_var[strlen(PATH)] == '=')
+			result = malloc(strlen(current_var) - strlen(PATH));
+		if (result == NULL)
+		{
+			perror("Memory allocation error");
+			exit(EXIT_FAILURE);
+		}
 		if (result != NULL)
 		{
-			strcpy(result, current_var + strlen(name) + 1);
+			strcpy(result, current_var + strlen(PATH) + 1);
 			break;
 		}
 	}
