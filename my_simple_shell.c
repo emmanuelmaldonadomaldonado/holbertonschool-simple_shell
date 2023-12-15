f51b1d69aa9b81a481683bb57adce544476415d7
Louis Toro <louistororosario@gmail.com>
2023-12-15 00:06:10 -0400

    remove lines of the code, some functions moved to other files, need to be fixed for betty

--- my_simple_shell.c
+++ my_simple_shell.c
@@ -1,125 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
-#define my_args 100 /* Define the maximum number of arguments */
-
-/* Function to print the shell prompt */
-void imprimir_prompt(int interactive)
-{
-	if (interactive)
-	{
-		char *dolar = "$";
-		printf("%s ", dolar);
-		fflush(stdout); /* Ensure prompt prints immediately */
-	}
-}
-
+#include "main.h"
+/*
+ */
 int main(void)
 {
-	int num_token, command_found;
-	size_t i;
-	char *argv[my_args]; /* Array to store command arguments */
-	char *command = NULL; /* Input command string */
-	char *token;
+	char *path[] = {"/usr/local/bin/", "/usr/bin/", "/bin/", "/usr/local/games/", "/usr/games/"}; /* Ruta predeterminada para la ejecución de comandos */
+	char my_path[my_args];
+	char *command = NULL; /* Cadena de comando de entrada */
+	char *argv[my_args];
+	extern char **environ; /* Copia de las variables de entorno */
 	size_t buffer = 0;
 	size_t length;
-	char *path[] = {"/usr/local/bin/", "/usr/bin/", "/bin/", "/usr/local/games/", "/usr/games/"}; /* Default path for command execution */
-	char my_path[my_args]; /* Store the constructed path for command execution */
-	int status;
-	pid_t pid;
-	extern char **environ; /* Copy of the environment variables */
+	int num_token;
+	int command_found;
+	int interactive = isatty(fileno(stdin)); /* Verificar si la terminal está en modo interactivo */
 
-	int interactive = isatty(fileno(stdin)); /* Check if shell is in interactive mode */
-
-	while (1) /*interactive mode */
+	while (1)
 	{
-		imprimir_prompt(interactive); /* Print the shell prompt */
-
-		/* Read command from user input */
+		imprimir_prompt(interactive); /* Imprimir el prompt de la terminal */
 		if (getline(&command, &buffer, stdin) != -1)
 		{
 			length = strlen(command);
 			if (command[length - 1] == '\n')
 				command[length - 1] = '\0';
-
-			if (strcmp(command, "") == 0) /* Check for empty command */
+			if (strcmp(command, "") == 0) /* Verificar comando vacío */
 				continue;
-
-			if (strcmp(command, "exit") == 0) /* Check for "exit" command to terminate the shell */
+			if (strcmp(command, "exit") == 0) /* Comprobar comando "exit" para terminar la terminal */
 			{
 				free(command);
-				return 0;
+				exit(0);
 			}
-
-			num_token = 0;
-			token = strtok(command, " ");
-			while (token != NULL && num_token < my_args - 1) /* Tokenize the command into arguments */
-			{
-				argv[num_token++] = token;
-				token = strtok(NULL, " ");
-			}
-			argv[num_token] = NULL;
-
-			if (num_token == 0) /* Check for no arguments provided */
+			num_token = tokenizer(command, argv);
+			if (num_token == 0) /* Verificar si no se proporcionaron argumentos */
 				continue;
-
-			command_found = 0; /* Flag to check if the command is found */
-			if (access(argv[0], X_OK) == 0) /* Check if the command is executable directly */
-			{
-				strcpy(my_path, argv[0]);
-				command_found = 1;
-			}
-			else
-			{
-				for (i = 0; i < 5; i++)
-				{
-					strcpy(my_path, path[i]); /* Construct the full path for the command */
-					strcat(my_path, argv[0]);
-					if (access(my_path, X_OK) == 0) /* Check if the command is executable in this path */
-					{
-						command_found = 1;
-						break;
-					}
-				}
-			}
-			/* If the command is not found, print an error and continue */
+			command_found = i_path(argv[0], path, my_path);
 			if (!command_found)
 			{
-				fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
+				fprintf(stderr, "./hsh: 1: %s: no se encontró\n", argv[0]);
 				free(command);
-				exit (127);
+				exit(127);
 			}
-			pid = fork(); /* Fork a new process for command execution */
-			if (pid == -1)
-			{
-				perror("fork failed");
-				exit(EXIT_FAILURE);
-			}
-			if (pid == 0) /* Child process executes the command */
-			{
-				/* Execute the command with arguments */
-				if (execve(my_path, argv, environ) == -1)
-				{
-					printf("Command execution failed\n");
-					exit(EXIT_FAILURE);
-				}
-			}
-			else /* Parent process waits for child process to complete */
-			{
-				waitpid(pid, &status, 0);
-			}
+			execute_order_66(my_path, argv, environ);
 		}
-		else /* If input is not read properly, break the loop */
+		else
 		{
 			if (!interactive)
 			{
 				break;
 			}
-			printf("\n"); /* If it is interactive and input is not read properly, print a new line */
+			printf("\n");
 			break;
 		}
 	}
