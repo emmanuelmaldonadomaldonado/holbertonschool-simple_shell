32ecd8ae9c4af82d4be6e431cf7de4ffa019cbdf
Louis Toro <louistororosario@gmail.com>
2023-12-15 00:40:15 -0400

    comment the code

--- my_simple_shell.c
+++ my_simple_shell.c
@@ -3,51 +3,50 @@
  */
 int main(void)
 {
-	char *path[] = {"/usr/local/bin/", "/usr/bin/", "/bin/", "/usr/local/games/", "/usr/games/"}; /* Ruta predeterminada para la ejecución de comandos */
-	char my_path[my_args];
-	char *command = NULL; /* Cadena de comando de entrada */
-	char *argv[my_args];
-	extern char **environ; /* Copia de las variables de entorno */
+	char *path[] = {"/usr/local/bin/", "/usr/bin/", "/bin/", "/usr/local/games/", "/usr/games/"}; /* Default path for command execution */
+	char my_path[my_args]; /* Store the constructed path for command execution */
+	char *command = NULL; /* Input command string */
+	char *argv[my_args]; /* Array to store command arguments */
 	size_t buffer = 0;
 	size_t length;
 	int num_token;
 	int command_found;
-	int interactive = isatty(fileno(stdin)); /* Verificar si la terminal está en modo interactivo */
+	int interactive = isatty(fileno(stdin)); /* Check if my_simple_shell is in interactive mode */
 
-	while (1)
+	while (1) /*interactive mode */
 	{
-		imprimir_prompt(interactive); /* Imprimir el prompt de la terminal */
-		if (getline(&command, &buffer, stdin) != -1)
+		imprimir_prompt(interactive); /* Print the shell prompt */
+		if (getline(&command, &buffer, stdin) != -1) /* Read command from user input */
 		{
 			length = strlen(command);
 			if (command[length - 1] == '\n')
 				command[length - 1] = '\0';
-			if (strcmp(command, "") == 0) /* Verificar comando vacío */
+			if (strcmp(command, "") == 0) /* Check for empty command */
 				continue;
-			if (strcmp(command, "exit") == 0) /* Comprobar comando "exit" para terminar la terminal */
+			if (strcmp(command, "exit") == 0) /* Check for "exit" command to terminate the shell */
 			{
 				free(command);
 				exit(0);
 			}
 			num_token = tokenizer(command, argv);
-			if (num_token == 0) /* Verificar si no se proporcionaron argumentos */
+			if (num_token == 0) /* Check for no arguments provided */
 				continue;
 			command_found = i_path(argv[0], path, my_path);
-			if (!command_found)
+			if (!command_found) /* If the command is not found, print an error and exit */
 			{
 				fprintf(stderr, "./hsh: 1: %s: no se encontró\n", argv[0]);
 				free(command);
 				exit(127);
 			}
-			execute_order_66(my_path, argv, environ);
+			execute_order_66(my_path, argv, environ); /* Execute the command with arguments */
 		}
-		else
+		else /* If input is not read properly, break the loop */
 		{
 			if (!interactive)
 			{
 				break;
 			}
-			printf("\n");
+			printf("\n"); /* If it is interactive and input is not read properly, print a new line */
 			break;
 		}
 	}
