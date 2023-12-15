47b9fdce8487cda2ee52d285c08fee95015aa2c0
Louis Toro <louistororosario@gmail.com>
2023-12-15 00:39:44 -0400

    comment the code

--- my_exec.c
+++ my_exec.c
@@ -7,23 +7,22 @@ void execute_order_66(char *my_path, char **argv, char **environ)
 	pid_t pid;
 	int status;
 
-	pid = fork(); /* Crear un nuevo proceso para la ejecución del comando */
+	pid = fork(); /* Fork a new process for command execution */
 	if (pid == -1)
 	{
-		perror("fork fallido");
+		perror("fork failed");
 		exit(EXIT_FAILURE);
 	}
-	if (pid == 0) /* El proceso hijo ejecuta el comando */
+	if (pid == 0) /* Child process executes the command */
 	{
-		/* Ejecutar el comando con los argumentos */
-		if (execve(my_path, argv, environ) == -1)
+		if (execve(my_path, argv, environ) == -1) /* Execute the command with arguments */
 		{
-			printf("Fallo en la ejecución del comando\n");
+			printf("Command execution failed\n");
 			exit(EXIT_FAILURE);
 		}
 	}
 	else
-	{ /* El proceso padre espera a que el proceso hijo termine */
-		waitpid(pid, &status, 0);
+	{
+		waitpid(pid, &status, 0); /* Parent process waits for child process to complete */
 	}
 }
